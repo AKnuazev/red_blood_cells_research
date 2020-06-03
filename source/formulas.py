313598e0ea7de86e887f1e215e3e974d14898ca2
AKnuazev <2536040@gmail.com>
2020-06-03 17:45:14 +0300

    Added formulas file and edited structure a bit

---
+++ source/formulas.py
@@ -0,0 +1,6 @@
+# Here you can write your functions formulas
+
+
+# Example
+def pow_2(x):
+    return x ** 2
---
+++ source/graphs.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from source.formulas import pow_2
+
+print("Enter x:")
+variable_name = input()
+print(variable_name)
+
+# Задаем зависимоти
+x = np.linspace(0, 0.1, 50)  # Массив из (p3) значений на отрезке (p1;p2)
+
+y1 = x          # Функция от заданных значений х
+y2 = pow_2(x)   # Вторая функция, заданная с использованием формулы, определенной в файле formulas
+
+# Построение графика
+plt.title("Зависимости")  # Заголовок
+plt.xlabel("x")           # Ось абсцисс (x)
+plt.ylabel("y1, y2")      # Ось ординат (y)
+plt.grid()                # Включение отображение сетки
+
+plt.plot(x, y1, x, y2)    # Построение графика (теперь он существует где то в памяти)
+plt.show()                # Вывод графика на экран
